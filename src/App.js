e590ea502c168a06b0212a8d126baa96ba6fbee0
kush desai <kush@artiumacademy.com>
2023-06-05 12:37:38 +0530

    add fastboard package"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useFastboard, Fastboard } from "@netless/fastboard-react";
+import React, { useEffect, useState } from "react";
+
 function App() {
+
+  // const [token, setToken] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('https://dspmd68lf6.execute-api.ap-south-1.amazonaws.com/roomToken')
+  //     .then((response) => response.json())
+  //     .then((res) => {
+  //       console.log("res==", res);
+  //       setToken(res);
+  //     })
+  // }, [])
+
+  const fastboard = useFastboard(() => ({
+    sdkConfig: {
+      appIdentifier: "lFsVUHd8EeyRoMGwMgqdqg/UP5q_u4F4Hjx-w",
+      region: "us-sv", // "cn-hz" | "us-sv" | "sg" | "in-mum" | "gb-lon"
+    },
+    joinRoom: {
+      uid: "11222",
+      uuid: "2916a270036a11eeb7c1615b55e934d3",
+      roomToken: "NETLESSROOM_YWs9VTJEVHVOYUFadGNRaU01SiZleHBpcmVBdD0xNjg1OTUwMDk5NzI5Jm5vbmNlPTE2ODU5NDY0OTk3MjkwMCZyb2xlPTAmc2lnPTA4OGU2YjY5ZmExYjYxYTcwZDMyMmExYThjMDU0MGNmODc0MzBhYmE4M2FhY2FkODA3MTAzOTA4NGNmOTU4Y2YmdXVpZD0yOTE2YTI3MDAzNmExMWVlYjdjMTYxNWI1NWU5MzRkMw",
+    },
+  }));
+
+  const FastboardUIConfig = { toolbar: { apps: { enable: false } } }
+  // Container must have a visible size
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      height: "400px",
+      border: "1px solid",
+      background: "#f1f2f3",
+    }}>
+      <Fastboard app={fastboard} config={FastboardUIConfig} />
     </div>
   );
 }
