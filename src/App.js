465ecc2c1447b821a13e358cee6ed2b22e468a31
kush desai <kush@artiumacademy.com>
2023-06-14 12:06:18 +0530

    add changes to fastboard dir

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ function App() {
       region: "us-sv", // "cn-hz" | "us-sv" | "sg" | "in-mum" | "gb-lon"
     },
     joinRoom: {
-      uid: "11222",
-      uuid: "2916a270036a11eeb7c1615b55e934d3",
-      roomToken: "NETLESSROOM_YWs9VTJEVHVOYUFadGNRaU01SiZleHBpcmVBdD0xNjg1OTUwMDk5NzI5Jm5vbmNlPTE2ODU5NDY0OTk3MjkwMCZyb2xlPTAmc2lnPTA4OGU2YjY5ZmExYjYxYTcwZDMyMmExYThjMDU0MGNmODc0MzBhYmE4M2FhY2FkODA3MTAzOTA4NGNmOTU4Y2YmdXVpZD0yOTE2YTI3MDAzNmExMWVlYjdjMTYxNWI1NWU5MzRkMw",
+      uid: "112",
+      uuid: "447eb0c00a7c11eea4399deea8dac2db",
+      roomToken: "NETLESSROOM_YWs9VTJEVHVOYUFadGNRaU01SiZleHBpcmVBdD0xNjg2NzI3NTM1MTE1Jm5vbmNlPTE2ODY3MjM5MzUxMTUwMCZyb2xlPTAmc2lnPTJiNTg0YzhlZjIwYTBkMDhhODc5ZTc4MDg3NzcyZDg0NjVkMzY1ZWQxZDc4NDYyZGI0ODk2NWE0MzkzZWUwMDcmdXVpZD00NDdlYjBjMDBhN2MxMWVlYTQzOTlkZWVhOGRhYzJkYg",
     },
   }));
 
